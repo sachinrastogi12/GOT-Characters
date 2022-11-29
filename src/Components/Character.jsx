9a3be05820f0ea7653c18dc15a24cad78011e130
sachinrastogipvt <sachinrastogipvt@gmail.com>
2022-11-29 15:47:24 +0530

    2nd

--- src/Components/Character.jsx
+++ src/Components/Character.jsx
@@ -3,9 +3,11 @@ import { Card } from "@mui/material";
 const Character = ({props}) =>{
     return (
        // <p>{props.characterName}</p>
+       props.characterImageFull ?  // condition for jo image available hai wahi dikha do
        <Card>
-        <img src = {props.characterImageFull} alt = "picture">
+        <img src = {props.characterImageFull} alt = "picture"/>
        </Card>
+       : ''
     )
 }
 export default Character;
